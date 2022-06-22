21a01e9ab33be6b3e7d484954a069c40f7a74c95
Travis <travisbly@gmail.com>
2022-06-21 21:49:38 -0700

    added login

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,35 +2,45 @@ const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
 
-// GET /api/set-token-cookie
-router.get(
-  '/set-token-cookie',
-  asyncHandler(async (req, res) => {
-    const user = await User.findOne({
-      where: {
-        email: 'demo@user.io',
-      },
-    });
-    setTokenCookie(res, user);
-    return res.json({ user });
-  })
-);
+// router.post('/test', (req, res) => {
+//   res.json({ requestBody: req.body });
+// });
 
-// GET /api/restore-user
-const { restoreUser } = require('../../utils/auth.js');
-router.get('/restore-user', restoreUser, (req, res) => {
-  return res.json(req.user);
-});
+// // GET /api/set-token-cookie
+// router.get(
+//   '/set-token-cookie',
+//   asyncHandler(async (req, res) => {
+//     const user = await User.findOne({
+//       where: {
+//         email: 'demo@user.io',
+//       },
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+//   })
+// );
 
-// GET /api/require-auth
-const { requireAuth } = require('../../utils/auth.js');
-router.get('/require-auth', requireAuth, (req, res) => {
-  return res.json(req.user);
-});
+// // GET /api/restore-user
+// const { restoreUser } = require('../../utils/auth.js');
+// router.get('/restore-user', restoreUser, (req, res) => {
+//   return res.json(req.user);
+// });
+
+// // GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get('/require-auth', requireAuth, (req, res) => {
+//   return res.json(req.user);
+// });
 
 module.exports = router;
